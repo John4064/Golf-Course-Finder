bb4b0ab1805cb47ee17d38087e97a38317bffa90
John Parkhurst <jparkhurst120@gmail.com>
2022-02-18 00:04:46 -0500

    Quick Basic Tests

--- src/test/java/com/parkhurst/golfcoursefinder/CourseTest.java
+++ src/test/java/com/parkhurst/golfcoursefinder/CourseTest.java
@@ -6,21 +6,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.parkhurst.golfcoursefinder.Course.*;
 class CourseTest {
-
+    Course testCourse;
     @BeforeEach
     void setUp() {
-
+        this.testCourse = new Course("Pebble Beach", "01010", 3.0F);
     }
 
     @Test
     void getName() {
+        assertEquals(this.testCourse.getName(),"Pebble Beach");
     }
 
     @Test
     void getZipcode() {
+        assertEquals(this.testCourse.getZipcode(),"01010");
     }
 
     @Test
     void getDistance() {
+        assertEquals(this.testCourse.getDistance(),3.0);
     }
 }
