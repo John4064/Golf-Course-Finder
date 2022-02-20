cf4f6dfdb5bddf4d95e41b9445d22a561dd2a1a4
John Parkhurst <jparkhurst120@gmail.com>
2022-02-19 21:03:08 -0500

    broken
    broken but setting up cellvaluefactories

--- src/main/java/com/parkhurst/golfcoursefinder/MainController.java
+++ src/main/java/com/parkhurst/golfcoursefinder/MainController.java
@@ -8,9 +8,11 @@ import com.parkhurst.golfcoursefinder.Course.*;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Properties;
 /**
  * @author John parkhurst
@@ -21,6 +23,13 @@ public class MainController {
     private Label welcomeText;
     @FXML
     private TableView<Course> table;
+    @FXML
+    private TableColumn<Course, String> nameCol;
+    @FXML
+    private TableColumn<Course, String> zipCol;
+    @FXML
+    private TableColumn<Course, String> distCol;
+
     @FXML
     protected void onHelloButtonClick() throws IOException {
         /*
@@ -40,6 +49,19 @@ public class MainController {
             TableRow<Course> row = new TableRow<>();
             return row;
         });
+        //table.getItems().addAll(new Course(("Winged foot","06820",3.2F));
+
+
+        table.getItems().addAll(new Course("Jacob", "Smith", 3),
+                new Course("Isabella", "Johnson", 4),
+                new Course("Ethan", "Williams", 5),
+                new Course("Emma", "Jones", 6),
+                new Course("Michael", "Brown", 7));
+        nameCol.setCellValueFactory(new PropertyValueFactory<Course, String>("courseName"));
+        zipCol.setCellValueFactory(new PropertyValueFactory<Course, String>("courseZip"));
+        distCol.setCellValueFactory(new PropertyValueFactory<Course, String>("distanceTo "));
+        table.getColumns().addAll(Arrays.asList(nameCol,zipCol,distCol));
+
         welcomeText.setText("Kobe Bryant in Prime");
     }
 }
