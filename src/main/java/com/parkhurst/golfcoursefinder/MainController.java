b807b8a7dc5794ebe1ff9611d0bd644384e3ca21
John Parkhurst <jparkhurst120@gmail.com>
2022-02-20 19:31:27 -0500

    Update MainController.java

--- src/main/java/com/parkhurst/golfcoursefinder/MainController.java
+++ src/main/java/com/parkhurst/golfcoursefinder/MainController.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Properties;
+
 /**
  * @author John parkhurst
  */
@@ -32,6 +33,17 @@ public class MainController {
     private TableColumn<Course, String> distCol;
 
     @FXML
+    /** @brief Initialize the window
+     */
+    public void initialize() {
+        String response = "{\"courses\": [{\"name\": \"Golf Links Course at Pebble Beach Golf Links\", \"zip_code\": \"93953\", \"distance\": 0.0}, {\"name\": \"Cypress Point Club\", \"zip_code\": \"93953\", \"distance\": 0.3}, {\"name\": \"Cypress Point Club\", \"zip_code\": \"93953\", \"distance\": 0.3}, {\"name\": \"Spyglass Hill Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.0}, {\"name\": \"Spyglass Hill Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.0}, {\"name\": \"Poppy Hills Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.2}, {\"name\": \"Dunes Course at Monterey Peninsula Country Club\", \"zip_code\": \"93953\", \"distance\": 2.2}, {\"name\": \"The Links at Spanish Bay\", \"zip_code\": \"93953\", \"distance\": 2.9}, {\"name\": \"West Course at Rancho Canada Golf Club\", \"zip_code\": \"93923\", \"distance\": 3.9}, {\"name\": \"Pacific Grove Municipal Golf Course\", \"zip_code\": \"93950\", \"distance\": 4.6}, {\"name\": \"Del Monte Golf Course\", \"zip_code\": \"93940\", \"distance\": 4.7}, {\"name\": \"Naval Postgraduate School Golf Course\", \"zip_code\": \"93940\", \"distance\": 5.0}, {\"name\": \"Quail Lodge Resort & Golf Club\", \"zip_code\": \"93923\", \"distance\": 6.0}, {\"name\": \"The Bayonet Course at Bayonet/Black Horse Golf Course\", \"zip_code\": \"93955\", \"distance\": 8.4}, {\"name\": \"Carmel Valley Ranch Resort\", \"zip_code\": \"93923\", \"distance\": 8.7}, {\"name\": \"Tehama Golf Club\", \"zip_code\": \"93923\", \"distance\": 8.8}, {\"name\": \"Laguna Seca Ranch Golf Club\", \"zip_code\": \"93940\", \"distance\": 9.0}, {\"name\": \"Pasadera Country Club\", \"zip_code\": \"93940\", \"distance\": 9.8}]}";
+        Hashtable <String,String> myHash = new Hashtable<String,String>();
+
+        //System.out.println(response("name"));
+    }
+
+
+    @FXML
     protected void onHelloButtonClick() throws IOException {
         /*
         //String temp = ApiHandling.courseLookup();
@@ -47,10 +59,7 @@ public class MainController {
         //PropertyHandler testHand = new PropertyHandler();
         //System.out.println(testHand.getPropValues());
         //System.out.println(ApiHandling.courseLookup());
-        String response = "{\"courses\": [{\"name\": \"Golf Links Course at Pebble Beach Golf Links\", \"zip_code\": \"93953\", \"distance\": 0.0}, {\"name\": \"Cypress Point Club\", \"zip_code\": \"93953\", \"distance\": 0.3}, {\"name\": \"Cypress Point Club\", \"zip_code\": \"93953\", \"distance\": 0.3}, {\"name\": \"Spyglass Hill Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.0}, {\"name\": \"Spyglass Hill Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.0}, {\"name\": \"Poppy Hills Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.2}, {\"name\": \"Dunes Course at Monterey Peninsula Country Club\", \"zip_code\": \"93953\", \"distance\": 2.2}, {\"name\": \"The Links at Spanish Bay\", \"zip_code\": \"93953\", \"distance\": 2.9}, {\"name\": \"West Course at Rancho Canada Golf Club\", \"zip_code\": \"93923\", \"distance\": 3.9}, {\"name\": \"Pacific Grove Municipal Golf Course\", \"zip_code\": \"93950\", \"distance\": 4.6}, {\"name\": \"Del Monte Golf Course\", \"zip_code\": \"93940\", \"distance\": 4.7}, {\"name\": \"Naval Postgraduate School Golf Course\", \"zip_code\": \"93940\", \"distance\": 5.0}, {\"name\": \"Quail Lodge Resort & Golf Club\", \"zip_code\": \"93923\", \"distance\": 6.0}, {\"name\": \"The Bayonet Course at Bayonet/Black Horse Golf Course\", \"zip_code\": \"93955\", \"distance\": 8.4}, {\"name\": \"Carmel Valley Ranch Resort\", \"zip_code\": \"93923\", \"distance\": 8.7}, {\"name\": \"Tehama Golf Club\", \"zip_code\": \"93923\", \"distance\": 8.8}, {\"name\": \"Laguna Seca Ranch Golf Club\", \"zip_code\": \"93940\", \"distance\": 9.0}, {\"name\": \"Pasadera Country Club\", \"zip_code\": \"93940\", \"distance\": 9.8}]}";
-        Hashtable <String,String> myHash = new Hashtable<String,String>();
 
-        System.out.println(response);
         welcomeText.setText("Kobe Bryant in Prime");
     }
     protected void setTable(){
@@ -60,8 +69,8 @@ public class MainController {
                 new Course("Willow Brook", "04321", 6),
                 new Course("Sterling Farms", "09001", 7));
         nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        zipCol.setCellValueFactory(new PropertyValueFactory<Course, String>("zipcode"));
-        distCol.setCellValueFactory(new PropertyValueFactory<Course, String>("distance"));
+        zipCol.setCellValueFactory(new PropertyValueFactory<>("zipcode"));
+        distCol.setCellValueFactory(new PropertyValueFactory<>("distance"));
     }
 
 }
