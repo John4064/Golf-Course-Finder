b94593e1efb776ea753dc644adcac3637e93d8b0
John Parkhurst <jparkhurst120@gmail.com>
2022-02-22 11:33:00 -0500

    Properties

--- src/main/java/com/parkhurst/golfcoursefinder/MainController.java
+++ src/main/java/com/parkhurst/golfcoursefinder/MainController.java
@@ -6,8 +6,11 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Hashtable;
+import java.util.Properties;
 
 /**
  * @author John parkhurst
@@ -31,7 +34,20 @@ public class MainController {
     public void initialize() {
         String response = "{\"courses\": [{\"name\": \"Golf Links Course at Pebble Beach Golf Links\", \"zip_code\": \"93953\", \"distance\": 0.0}, {\"name\": \"Cypress Point Club\", \"zip_code\": \"93953\", \"distance\": 0.3}, {\"name\": \"Cypress Point Club\", \"zip_code\": \"93953\", \"distance\": 0.3}, {\"name\": \"Spyglass Hill Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.0}, {\"name\": \"Spyglass Hill Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.0}, {\"name\": \"Poppy Hills Golf Course\", \"zip_code\": \"93953\", \"distance\": 1.2}, {\"name\": \"Dunes Course at Monterey Peninsula Country Club\", \"zip_code\": \"93953\", \"distance\": 2.2}, {\"name\": \"The Links at Spanish Bay\", \"zip_code\": \"93953\", \"distance\": 2.9}, {\"name\": \"West Course at Rancho Canada Golf Club\", \"zip_code\": \"93923\", \"distance\": 3.9}, {\"name\": \"Pacific Grove Municipal Golf Course\", \"zip_code\": \"93950\", \"distance\": 4.6}, {\"name\": \"Del Monte Golf Course\", \"zip_code\": \"93940\", \"distance\": 4.7}, {\"name\": \"Naval Postgraduate School Golf Course\", \"zip_code\": \"93940\", \"distance\": 5.0}, {\"name\": \"Quail Lodge Resort & Golf Club\", \"zip_code\": \"93923\", \"distance\": 6.0}, {\"name\": \"The Bayonet Course at Bayonet/Black Horse Golf Course\", \"zip_code\": \"93955\", \"distance\": 8.4}, {\"name\": \"Carmel Valley Ranch Resort\", \"zip_code\": \"93923\", \"distance\": 8.7}, {\"name\": \"Tehama Golf Club\", \"zip_code\": \"93923\", \"distance\": 8.8}, {\"name\": \"Laguna Seca Ranch Golf Club\", \"zip_code\": \"93940\", \"distance\": 9.0}, {\"name\": \"Pasadera Country Club\", \"zip_code\": \"93940\", \"distance\": 9.8}]}";
         Hashtable <String,String> myHash = new Hashtable<String,String>();
+        try (InputStream input = new FileInputStream("config.properties")) {
 
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("username"));
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         //System.out.println(response("name"));
     }
 
