b9fbb55c714090e1fabf42cf31d0083368100bd5
John Parkhurst <jparkhurst120@gmail.com>
2022-02-22 15:11:54 -0500

    Create Handler.java

---
+++ src/main/java/com/parkhurst/golfcoursefinder/Handler.java
@@ -0,0 +1,8 @@
+package com.parkhurst.golfcoursefinder;
+
+/**
+ * @author John Parkhurst
+ */
+//TODO: create a parent handler object for ziphandler, golfhandler, prophandler that they inherit from
+public class Handler {
+}
