79cad7cb4ce36685ee78e00a7ff5f0fcb4adc285
John Parkhurst <jparkhurst120@gmail.com>
2022-02-28 18:46:04 -0500

    Removeing Useless

--- src/test/java/com/parkhurst/golfcoursefinder/GolfHandlerTest.java
+++ src/test/java/com/parkhurst/golfcoursefinder/GolfHandlerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GolfHandlerTest {
 
